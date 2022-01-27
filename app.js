8a6070725a47d8027c39956ad4c619c9d769b87d
Jojan3990 <raijozan2443@gmail.com>
2022-01-27 20:39:29 +0545

    commit add

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port=process.env.PORT || 1234;
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
